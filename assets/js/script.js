9b4f0ec11293a1862dd444d94cdfd170a095ddb8
Duke Chizengwe <dr.duke260@gmail.com>
2024-09-29 21:53:00 +0200

    installed tailwind.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,42 +26,83 @@ for (let i = 0; i < navLinks.length; i++) {
  * header scroll active state & go to top
  */
 
-const goTopBtn = document.querySelector("[data-go-top]");
+// const goTopBtn = document.querySelector("[data-go-top]");
 
-window.addEventListener("scroll", function () {
+// window.addEventListener("scroll", function () {
 
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    goTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    goTopBtn.classList.remove("active");
-  }
+//   if (window.scrollY >= 100) {
+//     header.classList.add("active");
+//     goTopBtn.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//     goTopBtn.classList.remove("active");
+//   }
 
-});
-const testimonialsCarousel = document.querySelector('.testimonials-carousel');
-const slideWidth = testimonialsCarousel.offsetWidth;
-const slidesCount = testimonialsCarousel.children.length;
+// });
+// const testimonialsCarousel = document.querySelector('.testimonials-carousel');
+// const slideWidth = testimonialsCarousel.offsetWidth;
+// const slidesCount = testimonialsCarousel.children.length;
 
-let currentIndex = 0;
-let autoSlideInterval;
+// let currentIndex = 0;
+// let autoSlideInterval;
 
-function updateCarousel() {
+// function updateCarousel() {
     
-    currentIndex = (currentIndex + 1) % slidesCount;
+//     currentIndex = (currentIndex + 1) % slidesCount;
 
-    testimonialsCarousel.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
-}
+//     testimonialsCarousel.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+// }
 
-function startAutoSlide() {
-    autoSlideInterval = setInterval(() => {
-        updateCarousel();
-    }, 5000); 
-}
+// function startAutoSlide() {
+//     autoSlideInterval = setInterval(() => {
+//         updateCarousel();
+//     }, 5000); 
+// }
 
-function stopAutoSlide() {
-    clearInterval(autoSlideInterval);
-}
+// function stopAutoSlide() {
+//     clearInterval(autoSlideInterval);
+// }
 
-updateCarousel();
-startAutoSlide();
+// updateCarousel();
+// startAutoSlide();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:true,
+    centerMode: true,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // centerMode: true,
+
+      }
+
+    }, {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        infinite: true,
+
+      }
+    },  {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      }
+    }]
+  });
+});
